4b491e8fcaa4b68cc396486246be481070a50927
bastihz <41594266+BastiHz@users.noreply.github.com>
2019-12-31 01:19:42 +0100

    Add rules, switch for viewing mode, and decouple UPS from FPS

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 # - Profilen und dann die Geschwindigkeit verbessern. Ich will
 #   800 * 800 Zellen @ 60 FPS. Letzter Stand (ohne Nachbarn zu plotten):
-#   rund 44 FPS
+#   rund 44 FPS. Achtung: beim Testen die UPS nicht begrenzen!
 # - schnelles addieren ohne neues array zu erzeugen: np.add(a, b, out=a),
 #   zumindest laut dem surfarray Tutorial. Hilft das? Scheint nicht so.
-# - Geschwindigkeitstest machen. Ist das schneller als pg.draw.rect() oder
-#   blitten von vorbereiteten quadratischen Surfaces?
-# Erkenntnis: Ein 2d-surfarray läuft viel schneller als ein 3d.
-
-
-# Regeln aus Wikipedia
-# Any live cell with two or three neighbors survives.
-# Any dead cell with three live neighbors becomes a live cell.
-# All other live cells die in the next generation. Similarly, all other dead cells stay dead.
 
 
 import pygame as pg
 import numpy as np
 
+
+RULES = {
+    "birth": (3,),
+    "survive": (2, 3)
+}
 WORLD_WIDTH = 400
 WORLD_HEIGHT = 400
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
-SHOW_NEIGHBORS = True
+FPS = 60
+UPS = 20
+show_neighbors = True
 
 pg.init()
 window = pg.display.set_mode([WINDOW_WIDTH, WINDOW_HEIGHT])
@@ -40,10 +38,13 @@ visible_neighbors = np.zeros(visible_world.shape, int)
 # visible_world[5, 6] = 1
 # visible_world[5, 5] = 1
 # visible_world[4, 4] = 1
+
 visible_world = np.random.choice([0, 1], visible_world.shape, p=[0.9, 0.1])
 
-if SHOW_NEIGHBORS:
-    COLORS = [
+# Using a 2d surfarray is faster than a 3d surfarray. This, however, means that
+# the colors have to be converted to integers:
+COLORS = {
+    "rainbow": [
         int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
         int("{0:02x}{1:02x}{2:02x}".format(170, 0, 255), 16),
         int("{0:02x}{1:02x}{2:02x}".format(0, 0, 255), 16),
@@ -53,16 +54,18 @@ if SHOW_NEIGHBORS:
         int("{0:02x}{1:02x}{2:02x}".format(170, 255, 0), 16),
         int("{0:02x}{1:02x}{2:02x}".format(255, 170, 0), 16),
         int("{0:02x}{1:02x}{2:02x}".format(255, 0, 0), 16)
-    ]
-else:
-    COLORS = [
+    ],
+    "bw": [
         int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
         int("{0:02x}{1:02x}{2:02x}".format(255, 255, 255), 16)
     ]
+}
 
 running = True
+time_per_update = 1 / UPS * 1000  # milliseconds
+time_since_last_update = 0
 while running:
-    clock.tick(60)
+    dt = clock.tick(FPS)
 
     for event in pg.event.get():
         if event.type == pg.QUIT:
@@ -72,6 +75,13 @@ while running:
                 running = False
             elif event.key == pg.K_f:
                 print(clock.get_fps())
+            elif event.key == pg.K_s:
+                show_neighbors = not show_neighbors
+
+    time_since_last_update += dt
+    if time_since_last_update < time_per_update:
+        continue
+    time_since_last_update -= time_per_update
 
     big_world[1:-1, 1:-1] = visible_world
     big_world[0, 1:-1] = visible_world[-1, :]
@@ -91,20 +101,22 @@ while running:
     neighbors[:-1, 1:] += big_world[1:, :-1]
 
     temp_world[...] = 0
-    temp_world[np.logical_and(big_world == 0, neighbors == 3)] = 1
-    temp_world[np.logical_and(big_world == 1, np.logical_or(neighbors == 2, neighbors == 3))] = 1
+    mask = np.logical_or(
+        np.logical_and(big_world == 0, np.isin(neighbors, RULES["birth"])),
+        np.logical_and(big_world == 1, np.isin(neighbors, RULES["survive"]))
+    )
+    temp_world[mask] = 1
     visible_world[...] = temp_world[1:-1, 1:-1]
 
-    if SHOW_NEIGHBORS:
+    if show_neighbors:
         visible_neighbors[...] = neighbors[1:-1, 1:-1]
-        # FIXME: Geht das auch schneller?
         for i in range(9):
-            world_display[visible_neighbors == i] = COLORS[i]
+            # FIXME: Geht das auch schneller?
+            world_display[visible_neighbors == i] = COLORS["rainbow"][i]
     else:
         for i in range(2):
-            world_display[visible_world == i] = COLORS[i]
+            world_display[visible_world == i] = COLORS["bw"][i]
 
     pg.surfarray.blit_array(small_surf, world_display)
     pg.transform.scale(small_surf, [WINDOW_WIDTH, WINDOW_HEIGHT], window)
-
     pg.display.flip()
