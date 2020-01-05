e20ab9037fe6a78494dd08968c87a13b36ebb97d
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-01-05 17:19:49 +0100

    Minor modifications

--- main.py
+++ main.py
@@ -14,10 +14,10 @@ RULES = {
     "birth": (3,),
     "survive": (2, 3)
 }
-WORLD_SIZE = (800, 800)
+WORLD_SIZE = (400, 400)
 WINDOW_SIZE = (800, 800)
 FPS = 60
-show_neighbors = True
+show_neighbors = False
 
 pg.init()
 window = pg.display.set_mode(WINDOW_SIZE)
@@ -27,6 +27,7 @@ world_display = np.zeros(WORLD_SIZE, int)
 neighbors = np.zeros(WORLD_SIZE, int)
 
 # glider:
+# world = np.zeros(WORLD_SIZE, bool)
 # world[3, 6] = 1
 # world[4, 6] = 1
 # world[5, 6] = 1
@@ -37,23 +38,21 @@ world = np.random.choice([False, True], WORLD_SIZE, p=[0.9, 0.1])
 
 # Using a 2d surfarray is faster than a 3d surfarray. This, however, means that
 # the colors have to be converted to integers:
-COLORS = {
-    "rainbow": [
-        int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(170, 0, 255), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(0, 0, 255), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(0, 170, 255), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(0, 255, 170), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(0, 255, 0), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(170, 255, 0), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(255, 170, 0), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(255, 0, 0), 16)
-    ],
-    "bw": [
-        int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
-        int("{0:02x}{1:02x}{2:02x}".format(255, 255, 255), 16)
-    ]
-}
+COLORS_NEIGHBORS = [
+    int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(170, 0, 255), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(0, 0, 255), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(0, 170, 255), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(0, 255, 170), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(0, 255, 0), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(170, 255, 0), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(255, 170, 0), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(255, 0, 0), 16)
+]
+COLORS_DEAD_ALIVE = [
+    int("{0:02x}{1:02x}{2:02x}".format(0, 0, 0), 16),
+    int("{0:02x}{1:02x}{2:02x}".format(255, 255, 255), 16)
+]
 
 running = True
 ups = 60
@@ -133,12 +132,12 @@ while running:
     )
 
     if show_neighbors:
-        for i in range(9):
+        for n in range(9):
             # FIXME: Geht das auch schneller?
-            world_display[neighbors == i] = COLORS["rainbow"][i]
+            world_display[neighbors == n] = COLORS_NEIGHBORS[n]
     else:
-        world_display[world] = COLORS["bw"][1]
-        world_display[np.logical_not(world)] = COLORS["bw"][0]
+        world_display[world] = COLORS_DEAD_ALIVE[1]
+        world_display[np.logical_not(world)] = COLORS_DEAD_ALIVE[0]
 
     pg.surfarray.blit_array(small_surf, world_display)
     pg.transform.scale(small_surf, WINDOW_SIZE, window)
